a0a1fe175851a67621cf2cc0cbdf74b994a05503
mista-j <johnnie.a.hernandez@gmail.com>
2017-12-03 20:56:40 -0500

    unhuffman creates tree step 17
    asfd

--- unhuffman.cpp
+++ unhuffman.cpp
@@ -7,12 +7,58 @@
 // **Say what this program does here.  (replace this comment)**
 
 
+
 #include <cstdio>
+#include <cstring>
+#include <cctype>
+#include "trace.h"
+#include "tree.h"
+#include "pqueue.h"
+#include "binary.h"
+
 using namespace std;
 
+Tree binaryToTree(BFILE* f, int x);
+
+int traceEnabled = 0;
 
 int main(int argc, char** argv)
 {
-    
-  return 0;
+    if (argc > 2) {
+        if ( strcmp(argv[1], "-t") == 0 ) {
+            traceEnabled = 1;
+        } else {
+            printf("\n usage: huffman [-t] \n");
+            return 1;
+        }
+    }
+
+    const char* A;
+    A = argv[argc-1];    
+    BFILE* f = openBinaryFileRead(A);
+    Tree huffmanTree;
+
+    int temp = readBit(f);
+    if( temp != EOF){
+        huffmanTree = binaryToTree(f,  temp);
+    }
+
+    tracePrintTree(huffmanTree);
+
+    return 0;
+}
+
+
+Tree binaryToTree(BFILE* f,  int x){
+    if (x == 1){
+        Tree head = new Node(readByte(f));
+        return head;
+    } 
+    if (x == 0){
+        Tree head = new Node(binaryToTree(f, readBit(f)) , binaryToTree(f, readBit(f)));
+        return head;
+    }
+    if (x == EOF){
+        return NULL;
+    }
 }
